52a026bb1e4d001b989007682c7db2babef0e561
W3i537h <maatro_1001@hotmail.com>
2021-04-08 22:37:51 +0200

    Update netdiscovery.ps1

--- netdiscovery.ps1
+++ netdiscovery.ps1
@@ -2,6 +2,6 @@ function netconn() {
     Get-NetTCPConnection | where {$_.State -cmatch 'Established' -and $_.RemoteAddress -ne '127.0.0.1' -and $_.LocalAddress -cnotmatch '::'}
 }
 function netconn-resolve() {
-    $iplist = Get-NetTCPConnection | where {$_.RemoteAddress -ne '127.0.0.1' -and $_.State -cmatch 'stablished' -and $_.LocalAddress -cnotmatch '::'} | select RemoteAddress | foreach {$_.RemoteAddress}
+    $iplist = netconn | select RemoteAddress | foreach {$_.RemoteAddress}
     $iplist | foreach {Invoke-RestMethod -Uri http://ip-api.com/json/$_}
 }
