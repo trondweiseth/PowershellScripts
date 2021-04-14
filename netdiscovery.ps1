388f12f300e0686f462b6dda543ff3e3eac073f5
W3i537h <maatro_1001@hotmail.com>
2021-04-14 15:59:10 +0200

    Update netdiscovery.ps1

--- netdiscovery.ps1
+++ netdiscovery.ps1
@@ -1,7 +1,28 @@
-function netconn() {
-    Get-NetTCPConnection | where {$_.State -cmatch 'Established' -and $_.RemoteAddress -ne '127.0.0.1' -and $_.LocalAddress -cnotmatch '::'}
-}
-function netconn-resolve() {
-    $iplist = netconn | select RemoteAddress | foreach {$_.RemoteAddress}
-    $iplist | foreach {Invoke-RestMethod -Uri http://ip-api.com/json/$_}
+Function netconn() {
+
+    param(
+        [switch]$resolve,
+        [switch]$out
+    )
+    function getconnections() {
+        if ($out) {
+            Get-NetTCPConnection | Where-Object {$_.State -cmatch 'Established' -and $_.RemoteAddress -ne '127.0.0.1' -and $_.LocalAddress -cnotmatch '::'} | Out-GridView
+        } else {
+            Get-NetTCPConnection | Where-Object {$_.State -cmatch 'Established' -and $_.RemoteAddress -ne '127.0.0.1' -and $_.LocalAddress -cnotmatch '::'}
+        }
+    }
+    function resolver() {
+
+        $iplist = netconn | Select-Object RemoteAddress | ForEach-Object {$_.RemoteAddress}
+        if ($out) {
+            $iplist | ForEach-Object {Invoke-RestMethod -Uri http://ip-api.com/json/$_} | Out-GridView
+        } else {
+            $iplist | ForEach-Object {Invoke-RestMethod -Uri http://ip-api.com/json/$_ | Select-Object query,country,countryCode,region,regionName,city,zip,timezone,isp,org,as} | Format-Table -Autosize -Wrap
+        }
+    }
+    if ($resolve) {
+        resolver
+    } else {
+        getconnections
+    }
 }
