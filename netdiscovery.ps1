883e88940566cc437f6a56fff1c6f29e4fd53fd9
W3i537h <maatro_1001@hotmail.com>
2021-04-17 02:08:26 +0200

    Update netdiscovery.ps1

--- netdiscovery.ps1
+++ netdiscovery.ps1
@@ -1,4 +1,4 @@
-Function netconn() {
+Function nc() {
     <#
     .SYNOPSIS
     Check active connections
@@ -26,41 +26,60 @@ Function netconn() {
     #>
 
     param(
-        [switch]$resolve,
-        [switch]$out,
-        [switch]$getprocess,
-        [switch]$fullreport
+        [CmdletBinding()]
+        [switch]$Resolve,
+        [switch]$Out,
+        [switch]$GetProcess,
+        [switch]$Fullreport,
+
+        [Parameter(Mandatory=$false,ParameterSetName="Status")]
+        [ValidateSet('listen','established','bound','timeWait','closeWait')]
+        [string]$Status
     )
 
+    $nic = Get-NetAdapter | where {$_.Status -eq "Up"} | select -ExpandProperty Name
+    $localip = Get-NetIPAddress -InterfaceAlias $nic -AddressFamily IPv4 | select -ExpandProperty IPAddress
+
+    Function connquery() {
+        if ($Status) {
+            Get-NetTCPConnection -LocalAddress $localip -state $Status
+        } else {
+            Get-NetTCPConnection -LocalAddress $localip
+        }
+    }
     function fetchprocess() {
-        $pids = getconnections | Select-Object OwningProcess | ForEach-Object {$_.OwningProcess}
+        $pids = connquery | Select-Object -ExpandProperty OwningProcess
         $pids | ForEach-Object {Get-Process -Id $_ | Select-Object Id,ProcessName} | Format-Table
     }
     function getconnections() {
-        if ($out) {
-            Get-NetTCPConnection | Where-Object {$_.State -cmatch 'Established' -and $_.RemoteAddress -ne '127.0.0.1' -and $_.LocalAddress -cnotmatch '::'} | Out-GridView
+        if ($Out) {
+            connquery | Out-GridView
         } else {
-            Get-NetTCPConnection | Where-Object {$_.State -cmatch 'Established' -and $_.RemoteAddress -ne '127.0.0.1' -and $_.LocalAddress -cnotmatch '::'}
+            connquery
         }
     }
     function resolver() {
-        $iplist = netconn | Select-Object RemoteAddress | ForEach-Object {$_.RemoteAddress}
-        if ($out) {
+        if ($Status) {
+            $iplist = Get-NetTCPConnection -LocalAddress $localip -state $Status | select -ExpandProperty RemoteAddress
+        } else {
+            $iplist = Get-NetTCPConnection -LocalAddress $localip | select -ExpandProperty RemoteAddress
+        }
+        if ($Out) {
             $iplist | ForEach-Object {Invoke-RestMethod -Uri http://ip-api.com/json/$_} | Out-GridView
         } else {
             $iplist | ForEach-Object {Invoke-RestMethod -Uri http://ip-api.com/json/$_ | Select-Object query,country,countryCode,region,regionName,city,zip,timezone,isp,org,as} | Format-Table -Autosize -Wrap
         }
     }
 
-    if ($fullreport) {
+    if ($Fullreport) {
         getconnections
         fetchprocess
         resolver
     } else {
-        if ($getprocess){
+        if ($Getprocess){
             fetchprocess
         } else {
-            if ($resolve) {
+            if ($Resolve) {
                 resolver
             } else {
                 getconnections
