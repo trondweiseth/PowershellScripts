486175616a54936998bc099ab832aa8380530a66
W3i537h <maatro_1001@hotmail.com>
2021-08-12 16:33:39 +0200

    Update Net-Test.ps1

--- Net-Test/Net-Test.ps1
+++ Net-Test/Net-Test.ps1
@@ -20,12 +20,27 @@ Function Net-Test {
 
                 param([string]$rhost, [string]$port)
 
-                $ComputerName = Resolve-DnsName $rhost | Select-Object -ExpandProperty Name
+                $ComputerName = Resolve-DnsName $rhost | Select-Object -ExpandProperty Name -First 1
                 $ErrorActionPreference = "SilentlyContinue"
                 $netinterface = Get-NetIPInterface | Where-Object { $_.ConnectionState -eq "Connected" -and $_.AddressFamily -eq "IPv4" } | Select-Object -ExpandProperty InterfaceAlias -First 1
                 $srcip = Get-NetIPAddress -InterfaceAlias $netinterface -AddressFamily IPv4 | Select-Object -ExpandProperty IPAddress
                 $RA = Resolve-DnsName $rhost | ForEach-Object { $_.IPAddress }
-  
+                if ((Test-Connection localhost -Count 1 | Get-Member | foreach { $_.Name }) -imatch "Latency") {
+                    if ($responsetime = Test-Connection $rhost -Count 1 -ErrorAction SilentlyContinue  | Select-Object -ExpandProperty Latency) {
+                        $ping = "True"
+                    }
+                    else {
+                        $ping = "False"
+                    }
+                }
+                else {
+                    if ($responsetime = Test-Connection $rhost -Count 1 -ErrorAction SilentlyContinue  | Select-Object -ExpandProperty ResponseTime) {
+                        $ping = "True"
+                    }
+                    else {
+                        $ping = "False"
+                    }
+                }
                 function portinfotable() {
                     Write-Host -ForegroundColor Cyan "===================================="
                     Write-Host -NoNewline -ForegroundColor Green "CumputerName           : "; Write-Host -ForegroundColor Yellow "$ComputerName"
@@ -40,12 +55,6 @@ Function Net-Test {
                 }
 
                 if ($port) {
-                    if ($responsetime = Test-Connection $rhost -Count 1 -ErrorAction SilentlyContinue  | Select-Object -ExpandProperty ResponseTime) {
-                        $ping = "True"
-                    }
-                    else {
-                        $ping = "False"
-                    }
                     $socket = new-object System.Net.Sockets.TcpClient($rhost, $port)
                     If ($socket.Connected) {
                         $res = "True"
@@ -61,13 +70,6 @@ Function Net-Test {
     
                 }
                 else {
-    
-                    if ($responsetime = Test-Connection $rhost -Count 1 -ErrorAction SilentlyContinue  | Select-Object -ExpandProperty ResponseTime) {
-                        $ping = "True"
-                    }
-                    else {
-                        $ping = "False"
-                    }
                     Write-Host -ForegroundColor Cyan "===================================="
                     Write-Host -NoNewline -ForegroundColor Green "CumputerName           : "; Write-Host -ForegroundColor Yellow "$ComputerName"
                     Write-Host -NoNewline -ForegroundColor Green "RemoteAddress          : "; Write-Host -ForegroundColor Yellow "$RA"
@@ -81,12 +83,27 @@ Function Net-Test {
 
         }
         else {
-            $ComputerName = Resolve-DnsName $rhost | Select-Object -ExpandProperty Name
+            $ComputerName = Resolve-DnsName $rhost | Select-Object -ExpandProperty Name -First 1
             $netinterface = Get-NetIPInterface | Where-Object { $_.ConnectionState -eq "Connected" -and $_.AddressFamily -eq "IPv4" } | Select-Object -ExpandProperty InterfaceAlias -First 1
             $srcip = Get-NetIPAddress -InterfaceAlias $netinterface -AddressFamily IPv4 | Select-Object -ExpandProperty IPAddress
             $ErrorActionPreference = "SilentlyContinue"
             $RA = $(Resolve-DnsName $rhost | ForEach-Object { $_.IPAddress })
-
+            if ((Test-Connection localhost -Count 1 | Get-Member | foreach { $_.Name }) -imatch "Latency") {
+                if ($responsetime = Test-Connection $rhost -Count 1 -ErrorAction SilentlyContinue  | Select-Object -ExpandProperty Latency) {
+                    $ping = "True"
+                }
+                else {
+                    $ping = "False"
+                }
+            }
+            else {
+                if ($responsetime = Test-Connection $rhost -Count 1 -ErrorAction SilentlyContinue  | Select-Object -ExpandProperty ResponseTime) {
+                    $ping = "True"
+                }
+                else {
+                    $ping = "False"
+                }
+            }
             function portinfotable() {
                 
                 Write-Host -ForegroundColor Cyan "===================================="
@@ -102,12 +119,6 @@ Function Net-Test {
             }
 
             if ($port) {
-                if ($responsetime = Test-Connection $rhost -Count 1 -ErrorAction SilentlyContinue  | Select-Object -ExpandProperty ResponseTime) {
-                    $ping = "True"
-                }
-                else {
-                    $ping = "False"
-                }
                 $port | ForEach-Object {
                     $socket = new-object System.Net.Sockets.TcpClient($rhost, $_) 
                     If ($socket.Connected) {
@@ -125,13 +136,6 @@ Function Net-Test {
 
             }
             else {
-
-                if ($responsetime = Test-Connection $rhost -Count 1 -ErrorAction SilentlyContinue  | Select-Object -ExpandProperty ResponseTime) {
-                    $ping = "True"
-                }
-                else {
-                    $ping = "False"
-                }
                 Write-Host -ForegroundColor Cyan "===================================="
                 Write-Host -NoNewline -ForegroundColor Green "CumputerName           : "; Write-Host -ForegroundColor Yellow "$ComputerName"
                 Write-Host -NoNewline -ForegroundColor Green "RemoteAddress          : "; Write-Host -ForegroundColor Yellow "$RA"
