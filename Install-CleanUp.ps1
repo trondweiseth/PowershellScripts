c8bff42043873a0e7e525acf872c0d8f8d2297c1
W3i537h <maatro_1001@hotmail.com>
2021-08-17 15:15:54 +0200

    Update Install-CleanUp.ps1

--- Install-CleanUp.ps1
+++ Install-CleanUp.ps1
@@ -1,16 +1,16 @@
 Function Install-CleanUp() {
-param(
-[Parameter(Position=0,Mandatory=$true)][string]$softwarename
-)
-
-    $folderpaths = @(
-    'C:\Program Files\'
-    'C:\Program Files (x86)\'
-    'E:\Program Files\'
-    'E:\Program Files (x86)'
+    param(
+        [Parameter(Position = 0, Mandatory = $true)][string]$softwarename
     )
 
-    if ($registrykey = Get-ChildItem 'HKLM:\SOFTWARE\WOW6432Node\' | where {$_.Name -imatch $softwarename} | select -ExpandProperty PSPath) {
+    $folderpaths = @(
+        'C:\Program Files\'
+        'C:\Program Files (x86)\'
+        'E:\Program Files\'
+        'E:\Program Files (x86)'
+    )
+
+    if ($registrykey = Get-ChildItem 'HKLM:\SOFTWARE\WOW6432Node\' | Where-Object { $_.Name -imatch $softwarename } | Select-Object -ExpandProperty PSPath) {
         Write-Host -ForegroundColor Green -BackgroundColor Black "Registry key: $registrykey"
         Write-Host -ForegroundColor Yellow -BackgroundColor Black -NoNewline "Remove registry key? (Y\N):"
         $answer = Read-Host
@@ -18,21 +18,22 @@ param(
             Remove-Item $registrykey
             Write-Host -ForegroundColor Green "Registry key removed: $registrykey`n"
         }
-    } else {Write-Host -ForegroundColor Red "No matching registry keys."}
+    }
+    else { Write-Host -ForegroundColor Red "No matching registry keys." }
 
     foreach ($folderpath in $folderpaths) {
-        $softwarepath = Get-ChildItem -Path $folderpath | where {$_.Name -imatch "$softwarename"} | select -ExpandProperty FullName
+        $softwarepath = Get-ChildItem -Path $folderpath | Where-Object-Object { $_.Name -imatch "$softwarename" } | Select-Object -ExpandProperty FullName
         if ($softwarepath) {
             Write-Host -ForegroundColor Green -BackgroundColor Black "Found folder: $softwarepath"
             Write-Host -ForegroundColor Yellow -BackgroundColor Black -NoNewline "Remove folder? (Y\N):"
             $answer = Read-Host
             if ($answer -eq "y") {
-                #Remove-Item $softwarepath -Recurse
+                Remove-Item $softwarepath -Force -Recursive
                 Write-Host -ForegroundColor Red "Folder removed: $softwarepath"
             }
         }
     }
-	if (!$softwarepath) {
-		Write-Host -ForegroundColor Red "No matching folders."
-	}
+    if (!$softwarepath) {
+        Write-Host -ForegroundColor Red "No matching folders."
+    }
 }
