5d50dfb205a9fb554625560bdb24939bc5aefcc7
W3i537h <maatro_1001@hotmail.com>
2021-08-17 15:39:05 +0200

    Update Install-CleanUp.ps1

--- Install-CleanUp.ps1
+++ Install-CleanUp.ps1
@@ -3,6 +3,13 @@ Function Install-CleanUp() {
         [Parameter(Position = 0, Mandatory = $true)][string]$softwarename
     )
 
+    $registrypaths = @(
+        'HKEY_CURRENT_USER\SOFTWARE\'
+        'HKEY_LOCAL_MACHINE\SOFTWARE\'
+        'HKEY_USERS\.DEFAULT\Software\'
+        'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\'
+    )
+
     $folderpaths = @(
         'C:\Program Files\'
         'C:\Program Files (x86)\'
@@ -10,16 +17,19 @@ Function Install-CleanUp() {
         'E:\Program Files (x86)'
     )
 
-    if ($registrykey = Get-ChildItem 'HKLM:\SOFTWARE\WOW6432Node\' | Where-Object { $_.Name -imatch $softwarename } | Select-Object -ExpandProperty PSPath) {
-        Write-Host -ForegroundColor Green -BackgroundColor Black "Registry key: $registrykey"
-        Write-Host -ForegroundColor Yellow -BackgroundColor Black -NoNewline "Remove registry key? (Y\N):"
-        $answer = Read-Host
-        if ($answer -eq "y") {
-            Remove-Item $registrykey
-            Write-Host -ForegroundColor Green "Registry key removed: $registrykey`n"
+    foreach ($registrypath in $registrypaths) {
+        if ($registrykey = Get-ChildItem registry::$registrypath | Where-Object { $_.Name -imatch $softwarename } | Select-Object -ExpandProperty PSPath) {
+            if ($registrykey) {
+                Write-Host -ForegroundColor Green -BackgroundColor Black "Registry key: $registrykey"
+                Write-Host -ForegroundColor Yellow -BackgroundColor Black -NoNewline "Remove registry key? (Y\N):"
+                $answer = Read-Host
+                if ($answer -eq "y") {
+                    Remove-Item $registrykey
+                    Write-Host -ForegroundColor Red "Registry key removed: $registrykey`n"
+                }
+            }
         }
     }
-    else { Write-Host -ForegroundColor Red "No matching registry keys." }
 
     foreach ($folderpath in $folderpaths) {
         $softwarepath = Get-ChildItem -Path $folderpath | Where-Object { $_.Name -imatch "$softwarename" } | Select-Object -ExpandProperty FullName
@@ -28,12 +38,16 @@ Function Install-CleanUp() {
             Write-Host -ForegroundColor Yellow -BackgroundColor Black -NoNewline "Remove folder? (Y\N):"
             $answer = Read-Host
             if ($answer -eq "y") {
-                Remove-Item $softwarepath -Force -Recursive
+                Remove-Item $softwarepath -Force
                 Write-Host -ForegroundColor Red "Folder removed: $softwarepath"
             }
         }
     }
+    
     if (!$softwarepath) {
         Write-Host -ForegroundColor Red "No matching folders."
+    }
+    elseif (!registrykey) {
+        Write-Host -ForegroundColor Red "No matching registry key."
     }
 }
