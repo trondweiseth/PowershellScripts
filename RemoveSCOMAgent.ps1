07df6e9755ce7b8e234288b7e740ce9bcf58153e
W3i537h <maatro_1001@hotmail.com>
2021-08-26 14:14:44 +0200

    Update RemoveSCOMAgent.ps1

--- RemoveSCOMAgent.ps1
+++ RemoveSCOMAgent.ps1
@@ -1,14 +1,14 @@
 Function RemoveSCOMAgent() {
 
-    param([string[]]$Hosts,[switch]$Validate)
-    if (!$Hosts) {Write-Host -ForegroundColor Yellow -BackgroundColor Black "SYNTAX: RemoveSCOMAgent -Hosts host1,host2`n";break}
+    param([string[]]$Hosts, [switch]$Validate)
+    if (!$Hosts) { Write-Host -ForegroundColor Yellow -BackgroundColor Black "SYNTAX: RemoveSCOMAgent -Hosts host1,host2`n"; break }
     [void](Import-Module OperationsManagerExtensions)
-    cls
+    Clear-Host
     Write-Host -ForegroundColor Yellow -BackgroundColor Black "Searching for SCOM Agent(s)..."
-    foreach ($Computername in $Hosts){
+    foreach ($Computername in $Hosts) {
         try {
             Remove-SCOMAgent -DNSHostname $Computername -ErrorAction Stop
-            if ($validate) {Remove-SCOMAgent -DNSHostname $Computername -ErrorAction Stop}
+            if ($validate) { Remove-SCOMAgent -DNSHostname $Computername -ErrorAction Stop }
         }
         catch {
             Write-Warning $Error[0]
