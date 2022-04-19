727569306bf60a18341226ddd19f0db3c32ac032
W3i537h <maatro_1001@hotmail.com>
2022-04-19 13:15:47 +0200

    Update service.ps1

--- service.ps1
+++ service.ps1
@@ -1,2 +1,2 @@
 # One-liner to search for services
-while ($true) { $i = read-host "Service" ; Get-Service | select Status,Name,DisplayName,StartType,DependentServices | Where-Object { $_.DisplayName -imatch "$i" -or $_.Name -imatch "$i" } | Format-Table -AutoSize }
+while ($true) { $i = read-host "Service" ; Get-Service | Select-Object Status,Name,DisplayName,StartType,DependentServices | Where-Object { $_.DisplayName -imatch "$i" -or $_.Name -imatch "$i" } | Format-Table -AutoSize }
