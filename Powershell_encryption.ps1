a0ef693e6f91adc63af9a4c9a00cdf8a9751f1a4
W3i537h <maatro_1001@hotmail.com>
2022-11-04 15:49:36 +0100

    Update Powershell_encryption.ps1

--- Powershell_encryption.ps1
+++ Powershell_encryption.ps1
@@ -14,32 +14,27 @@ Function Encrypt {
     )
 
     $item = Get-Item $File
-    $text = Get-Content $File
-    $FilePath = $item.Directory
-    if ($File -imatch '_decrypted' -or $File -imatch '_encrypted') {
-        $FileName = $item.BaseName.Split('_')[0]
-    }
-    else {
-        $FileName = $item.BaseName
-    }
+    $Data = Get-Content $File
+    $FullName = $item.FullName
+
     if ($Action -eq 'Encrypt') {
         $text = Get-Content $File
-        $encrypted = $text | ConvertTo-SecureString -AsPlainText -Force
+        $encrypted = $Data | ConvertTo-SecureString -AsPlainText -Force
         $fromsecurestring = $encrypted | ConvertFrom-SecureString
-        $fromsecurestring | Out-File ${FilePath}${FileName}_encrypted.txt
+        $fromsecurestring | Out-File $FullName
         if (!$Silent) {
-            Write-Host -f Green "Encrypted file saved at ${FilePath}${FileName}_encrypted.txt"
+            Write-Host -f Green "Encrypted file $FullName"
         }
     }
 
     if ($Action -eq 'Decrypt') {
-        $tosecurestring = Get-Content ${FilePath}${FileName}_encrypted.txt | ConvertTo-SecureString
+        $tosecurestring = Get-Content $FullName | ConvertTo-SecureString
         $tosecurestring | % {
             $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($_)
             $Marshal::PtrToStringAuto($Bstr)
-            } | Out-File ${FilePath}${FileName}_decrypted.txt
+            } | Out-File $FullName
             if (!$Silent) {
-                Write-Host -f Green "Decrypted file saved at ${FilePath}${FileName}_decrypted.txt"
+                Write-Host -f Green "Decrypted file $FullName"
             }
     }
 }
@@ -53,18 +48,7 @@ function Run-EncryptedScript {
     )
     
     $item = Get-Item $File
-    $FilePath = $item.Directory
-    $FileName = $item.BaseName.Split('_')[0]
     $FullName = $item.FullName
-    if (Test-Path ${FilePath}${FileName}_encrypted.txt) {
-        echo "Encrypted script. Use Run-EncryptedScript $FullName" > $FullName
-    }
-    else {
-        Encrypt $FullName -Action Encrypt -Silent
-    }
     Encrypt ${FilePath}${FileName}_encrypted.txt -Action Decrypt -Silent
-    cat ${FilePath}${FileName}_decrypted.txt > $FullName
-    rm ${FilePath}${FileName}_decrypted.txt
     & $FullName
-    echo "Encrypted script. Use Run-EncryptedScript $FullName" > $FullName
 }
